ebb8212a4f1beeba4e77d7bd819c5a24d29e0ccf
Dorina Kamp <dorina.kamp@gmail.com>
2022-06-24 15:38:39 +0200

    Adding ExpensesList uit Expenses to make code leaner; als een nieuwe jsx getoond moet worden is een seperate component beter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,17 +22,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.length === 0  && <p>No expenses found</p>}
-        {filteredExpenses.length > 0 &&
-           filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-           ))
-         }
+     <ExpensesList items={filteredExpenses}/>
+         
         {/* /* {[<Card />, <Card />]} */
         /* <div className="expenses">
           <ExpenseItem

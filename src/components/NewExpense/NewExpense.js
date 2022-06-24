b86ae59adffc138e08df800e2b8d07b10e1d4779
Dorina Kamp <dorina.kamp@gmail.com>
2022-06-24 16:38:03 +0200

    adding button and useState - its shown, when not eddited and versa vi

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
+import React, { useState } from "react";
+
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-  <div>
-    
-  </div>
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -13,11 +15,15 @@ const NewExpense = (props) => {
     props.onAddExpense(expenseData)
   };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
 
   return (
     <div className="new-expense">
-      <button>Add New Expense</button>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />}
     </div>
   );
 };
